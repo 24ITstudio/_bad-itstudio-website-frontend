5630fba8e603114d48ad122366055ae4ac5d3f2c
李德儒 <2060824762@qq.com>
2024-05-30 19:02:37 +0800

    评论页

---
+++ src/components/msagPage.vue
@@ -0,0 +1,185 @@
+<template>
+    <!-- <div class="element"> -->
+    <!-- <div class="slide-wrapper"> -->
+    <!-- <div class="slide"> -->
+    <!-- <div class="overlap-2"> -->
+    <!-- <div>这是Overlap</div>
+                    <div>这是OverlapGroup</div> -->
+    <!-- <Overlap /> -->
+    <!-- <OverlapGroup /> -->
+    <!-- </div> -->
+    <!-- </div> -->
+    <!-- </div> -->
+    <!-- </div> -->
+    <div class="back">
+        <div class="head">
+            导航栏组件
+        </div>
+        <div class="content">
+            <div class="back_left">
+                <div class="left_board">
+                    <div class="board_head">
+                        <div class="text_1">
+                            留言板
+                        </div>
+                    </div>
+                    <div class="board_content">
+                        <textarea class="dotted-textarea" rows="10"></textarea>
+                    </div>
+
+                </div>
+            </div>
+            <div class="back_right">
+                留言块
+            </div>
+        </div>
+
+
+    </div>
+</template>
+
+<script>
+// import { defineProps } from "vue";
+// import Overlap from "./Overlap.vue";
+// import OverlapGroup from "./OverlapGroup.vue";
+
+export default {
+    name: "msagPage",
+    components: {
+        // Overlap,
+        // OverlapGroup,
+    },
+};
+</script>
+<!-- 单选10 2
+不定项 10 3
+主观 2 50分 -->
+
+<style>
+.back{
+    background-color:beige;
+    /* width: 1920px; */
+    /* height: 1080px; */
+    width: 100%;
+    height: 100vh;
+    background-image: url('../assets/backImg.jpg');
+    background-size: 100% 100%;
+    position: fixed;
+    display: flex;
+    flex-direction: column;
+}
+.content{
+    height: 90%;
+    margin-top: 2%;
+    display: flex;
+    flex-direction: row;
+}
+.back_left{
+    background-color: #04132c;
+    width: 30%;
+    height: 100%;
+    /* width: 578px; */
+    /* height: 1080px; */
+    /* margin-top: px; */
+    margin-left: 0%;
+    margin-right: 0%;
+    /* margin-top: -10%; */
+}
+
+.left_board{
+    background-color: #d9d9d9;
+    width: 50%;
+    height: 70%;
+    margin-left: 40%;
+    margin-top: 20%;
+    
+
+    /* width: 351px; */
+    /* height: 695px; */
+    /* margin-left: 137px; */
+    
+    /* margin: 150px 90px 235px 137px; */
+    /* margin-top: 40px; */
+}
+
+.board_head{
+    height: 70px;
+    line-height: 80px;
+}
+
+.text_1{
+    margin-left: -100px;
+    font-size: 25px;
+    font-family: "Microsoft New Tai Lue-Bold", Helvetica;
+    font-weight: bold;
+}
+
+.board_content{
+    display: flex;
+        justify-content: center;
+        align-items: center;
+        /* height: 100vh; */
+}
+
+.dotted-textarea {
+    width: 100%;
+    /* max-width: 600px; */
+    /* height: 200px; */
+    padding: 10px;
+    /* outline: none; */
+    border: none;
+    resize: none;
+    font-size: 16px;
+    line-height: 1.5;
+    /* 调整行间距 */
+    background: repeating-linear-gradient(to bottom,
+            transparent,
+            transparent 28px,
+            #000 28px,
+            #000 29px);
+    /* 横线样式 */
+}
+
+
+.back_right {
+    background-color:#d9d9d91a;
+    width: 70%;
+    height: 100%;
+    /* width: 1342px; */
+    /* height: 1080px; */
+}
+
+
+
+
+
+.element {
+    background-color: transparent;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    width: 100%;
+}
+
+.element .slide-wrapper {
+    height: 1080px;
+    width: 1920px;
+    background-color: aqua;
+}
+
+.element .slide {
+    /* background-image: url(./slide-16-9-2.svg); */
+    background-position: 50% 50%;
+    background-size: cover;
+    height: 1080px;
+    overflow: hidden;
+    /* background-color: blueviolet; */
+}
+
+.element .overlap-2 {
+    height: 1080px;
+    position: relative;
+    width: 1923px;
+}
+
+</style>
---
+++ src/components/sign-up.vue
@@ -0,0 +1,146 @@
+<template>
+    <div class="body">
+        <div class="putIn">
+            <div class="head">
+                <img src="../assets/Go Back.png" alt="">
+                <div class="text" style="display: inline-block;">信息填报</div>
+            </div>
+            <form action="">
+                <div class="name fill">
+                    <img src="../assets/User.png" alt="" style="height: 30px;">
+                    <input type="text" placeholder="姓名">
+                </div>
+                <div class="stuId fill">
+                    <img src="../assets/Identification Documents.png" alt="" style="height: 30px;">
+                    <input type="text" placeholder="学号">
+                </div>
+                <div class="tele fill">
+                    <img src="../assets/Phone iphone.png" alt="" style="height: 30px;">
+                    <input type="text" placeholder="电话">
+                </div>
+                <div class="QQ fill">
+                    <img src="../assets/Flutter dash.png" alt="" style="height: 30px;">
+                    <input type="text" placeholder="QQ号">
+                </div>
+                <button>提&nbsp;&nbsp;&nbsp;&nbsp;交</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+* {
+    margin: 0;
+    padding: 0;
+}
+
+body {
+    margin: 0;
+}
+
+input {
+    outline: none;
+    border: 0;
+    background-color: transparent;
+}
+
+.body {
+    background-color: transparent;
+    background-image: url(../assets/background.png);
+    background-size: 100%;
+    display: flex;
+    position: relative;
+    height: 100vh;
+}
+
+.body .putIn {
+    width: 33.9%;
+    height: 65.19%;
+    background-color: #D9D9D9;
+    position: absolute;
+    top: 17.4%;
+    left: 11.25%;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+}
+
+.putIn .head {
+    width: 84.79%;
+    height: 55px;
+    margin-top: 8.23%;
+    display: flex;
+    margin-bottom: 9.5%;
+}
+
+.putIn .head .text {
+    color: var(--2, #04132D);
+    font-family: "Microsoft JhengHei UI";
+    font-size: 36px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 55px;
+    height: 55px;
+    margin-left: 28px;
+}
+
+.putIn form {
+    width: 84.79%;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    transform: translateY(-30px);
+}
+
+.putIn form .fill {
+    background-color: rgba(103, 110, 123, 0.09);
+    width: 100%;
+    height: 45px;
+    margin-bottom: 30px;
+    line-height: 45px;
+    display: flex;
+    align-items: center;
+    border-radius: 24px;
+    border: 2px solid #808DA5;
+}
+
+.putIn form .fill input {
+    line-height: 45px;
+    color: var(--2, #04132D);
+    font-family: "Microsoft JhengHei UI";
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 700;
+}
+
+.putIn form .fill input::placeholder {
+    color: #808DA5;
+}
+
+.putIn form .fill img {
+    margin-left: 15px;
+    margin-right: 20px;
+}
+
+.putIn form button {
+    color: #FFF;
+    font-family: "Microsoft JhengHei UI";
+    font-size: 32px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    height: 50px;
+    width: 60%;
+    border: none;
+    border-radius: 52px;
+    background: var(--2, #04132D);
+}
+
+.putIn form button:active {
+    opacity: 0.7;
+}
+</style>
