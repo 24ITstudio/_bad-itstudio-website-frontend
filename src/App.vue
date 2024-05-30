9f546b77e4be425726f8b1eb04827a65c982924a
李德儒 <2060824762@qq.com>
2024-05-30 19:02:37 +0800

    报名页完成60%

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+<signUp></signUp>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+import signUp from "./components/sign-up.vue";
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    signUp,
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  padding: 0;
 }
 </style>
