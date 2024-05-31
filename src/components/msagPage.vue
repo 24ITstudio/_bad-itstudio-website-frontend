443f11922e72749d46f58720602efd37d403287c
li <2633290579@qq.com>
2024-05-31 20:09:49 +0800

    评论页left_init

--- src/components/msagPage.vue
+++ src/components/msagPage.vue
@@ -1,16 +1,4 @@
 <template>
-    <!-- <div class="element"> -->
-    <!-- <div class="slide-wrapper"> -->
-    <!-- <div class="slide"> -->
-    <!-- <div class="overlap-2"> -->
-    <!-- <div>这是Overlap</div>
-                    <div>这是OverlapGroup</div> -->
-    <!-- <Overlap /> -->
-    <!-- <OverlapGroup /> -->
-    <!-- </div> -->
-    <!-- </div> -->
-    <!-- </div> -->
-    <!-- </div> -->
     <div class="back">
         <div class="head">
             导航栏组件
@@ -24,7 +12,13 @@
                         </div>
                     </div>
                     <div class="board_content">
-                        <textarea class="dotted-textarea" rows="10"></textarea>
+                        <textarea ref="input" v-bind="{placeholder: isPlaceholder}" v-model="content"
+                            @focus="clearPlaceholder" @blur="setPlaceholder" @input="checkInput" rows="9">
+                            有什么想说的，就在这里留下吧~
+                        </textarea>
+                    </div>
+                    <div class="submitB">
+                        <button class="inner">发&nbsp;&nbsp;&nbsp;&nbsp;布</button>
                     </div>
 
                 </div>
@@ -39,27 +33,36 @@
 </template>
 
 <script>
-// import { defineProps } from "vue";
-// import Overlap from "./Overlap.vue";
-// import OverlapGroup from "./OverlapGroup.vue";
-
 export default {
     name: "msagPage",
-    components: {
-        // Overlap,
-        // OverlapGroup,
+    data(){
+        return{
+            content: '有什么想说的，就在这里留下吧~',
+            isPlaceholder: true
+        }
     },
+    methods:{
+        clearPlaceholder(){
+            if(this.isPlaceholder){
+                this.content = '';
+                this.isPlaceholder = false;
+            }
+        },
+        setPlaceholder(){
+            if(this.content === ''){
+                this.content = '有什么想说的，就在这里留下吧~';
+                this.placeholder = true;
+            }
+        }
+    }
+    
 };
 </script>
-<!-- 单选10 2
-不定项 10 3
-主观 2 50分 -->
 
-<style>
+
+<style scoped>
 .back{
     background-color:beige;
-    /* width: 1920px; */
-    /* height: 1080px; */
     width: 100%;
     height: 100vh;
     background-image: url('../assets/backImg.jpg');
@@ -78,19 +81,15 @@ export default {
     background-color: #04132c;
     width: 30%;
     height: 100%;
-    /* width: 578px; */
-    /* height: 1080px; */
-    /* margin-top: px; */
     margin-left: 0%;
     margin-right: 0%;
-    /* margin-top: -10%; */
 }
 
 .left_board{
     background-color: #d9d9d9;
-    width: 50%;
-    height: 70%;
-    margin-left: 40%;
+    width: 60%;
+    height: 75%;
+    margin-left: 30%;
     margin-top: 20%;
     
 
@@ -104,82 +103,109 @@ export default {
 
 .board_head{
     height: 70px;
-    line-height: 80px;
+    line-height: 100px;
+    margin-left: 10%;
+    /* margin-top: 20px; */
+    /* border: 1px, solid, #04132c; */
+    /* margin-top: 5%; */
 }
 
 .text_1{
-    margin-left: -100px;
+    /* margin-left: 10px; */
     font-size: 25px;
     font-family: "Microsoft New Tai Lue-Bold", Helvetica;
     font-weight: bold;
+    /* color: #d9d9d9; */
 }
 
 .board_content{
     display: flex;
-        justify-content: center;
-        align-items: center;
-        /* height: 100vh; */
+    justify-content: center;
+    align-items: center;
+    height: 50%;
+    margin-left: 10%;
+    margin-right: 10%;
+    height: 73%;
+    margin-top: 9%;
+    /* border: 1px, solid, #04132c; */
 }
 
-.dotted-textarea {
+.placeholder {
+    color: #999;
+}
+
+textarea {
     width: 100%;
-    /* max-width: 600px; */
-    /* height: 200px; */
+    height: 100%;
+    /* height: calc(1.5em * 9);   */
+    line-height: 2.7em; 
+    border: none; 
+    outline: none; 
+    resize: none; 
+    background: none;
+    padding: 0;
+    margin: 0;
+    font-family: inherit; 
+    overflow: hidden;
+    /* border-bottom: 1px solid rebeccapurple; */
+}
+
+textarea::placeholder {
+    color: #999;
+}
+
+textarea:focus::placeholder {
+    color: transparent; 
+}
+
+.submitB{
+    width: 28%;
+    height: 4%;
+    line-height: 3%;
+    border-radius: 100px;
+    margin-left: 62%;
+    
+    background-color: #04132c;
+}
+
+.submitB .inner{
+    background-color: transparent;
+    color: #ffffff;
+    height: 100%;
+    border: none;
+    margin-left: 18%;
+    font-size: 12px;
+    font-weight: bold;
+    font-family: "Microsoft New Tai Lue-Bold", Helvetica;
+    
+}
+
+/* textarea { */
+    /* background-image: linear-gradient(to bottom, transparent 1.5em, #ccc 1.5em); */
+    /* background-size: 100% 1.5em; */
+    /* background-repeat: repeat-y; */
+/* } */
+
+/* .dotted-textarea {
+    width: 100%;
+    height: 350px;
     padding: 10px;
-    /* outline: none; */
     border: none;
     resize: none;
     font-size: 16px;
     line-height: 1.5;
-    /* 调整行间距 */
     background: repeating-linear-gradient(to bottom,
             transparent,
             transparent 28px,
             #000 28px,
             #000 29px);
-    /* 横线样式 */
-}
+} */
 
 
 .back_right {
     background-color:#d9d9d91a;
     width: 70%;
     height: 100%;
-    /* width: 1342px; */
-    /* height: 1080px; */
-}
-
-
-
-
-
-.element {
-    background-color: transparent;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    width: 100%;
-}
-
-.element .slide-wrapper {
-    height: 1080px;
-    width: 1920px;
-    background-color: aqua;
-}
-
-.element .slide {
-    /* background-image: url(./slide-16-9-2.svg); */
-    background-position: 50% 50%;
-    background-size: cover;
-    height: 1080px;
-    overflow: hidden;
-    /* background-color: blueviolet; */
-}
-
-.element .overlap-2 {
-    height: 1080px;
-    position: relative;
-    width: 1923px;
 }
 
 </style>
